88058fccb845d37ddafb4945ebd76b2799bb3557
Alexander Lebedev <itskilobyte@gmail.com>
2025-11-14 16:18:16 +0500

    Добавил контроллер-обёртку для пользователей

---
+++ backend/src/main/java/ru/serp/corpwish/users/UserController.java
@@ -0,0 +1,39 @@
+package ru.serp.corpwish.users;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User get(@PathVariable Long id) {
+        return userService.get(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+}
