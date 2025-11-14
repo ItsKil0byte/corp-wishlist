e450c2a990921223d1fa587ba51bf109badb81f2
Alexander Lebedev <itskilobyte@gmail.com>
2025-11-14 15:55:56 +0500

    Добавил сервис для пользователей

---
+++ backend/src/main/java/ru/serp/corpwish/users/UserService.java
@@ -0,0 +1,39 @@
+package ru.serp.corpwish.users;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User get(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(Long id, User update) {
+        User user = get(id);
+
+        user.setLogin(update.getLogin());
+        user.setPassword(update.getPassword());
+        user.setFullName(update.getFullName());
+
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
