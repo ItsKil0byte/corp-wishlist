764f749279201432ab00947eecf6383059891c8e
Alexander Lebedev <itskilobyte@gmail.com>
2025-11-23 14:35:42 +0500

    Обновил пользовательский репозиторий

--- backend/src/main/java/ru/serp/corpwish/users/UserRepository.java
+++ backend/src/main/java/ru/serp/corpwish/users/UserRepository.java
@@ -2,7 +2,8 @@ package ru.serp.corpwish.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Boolean existsByLogin(String login);
-    // User findByLogin(String login);
+    Optional<User> findByUsername(String username);
 }
