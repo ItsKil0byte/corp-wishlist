d02e3f574f3d9bab0723e087d211446723000a50
Alexander Lebedev <itskilobyte@gmail.com>
2025-11-27 16:29:04 +0500

    Сменил опцию в конфиге безопасности для отправки запросов

--- backend/src/main/java/ru/serp/corpwish/security/SecurityConfig.java
+++ backend/src/main/java/ru/serp/corpwish/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .headers(headers ->
                 headers.frameOptions(
-                    HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                    HeadersConfigurer.FrameOptionsConfig::disable
                 )
             )
             .authorizeHttpRequests(authorize ->
