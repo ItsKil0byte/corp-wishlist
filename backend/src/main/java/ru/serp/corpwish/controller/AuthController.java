655cfe7abe463b75706cdedfc3321b55b24965b1
Alexander Lebedev <itskilobyte@gmail.com>
2025-11-27 14:38:16 +0500

    Добавил страницу для теста работы авторизации

--- backend/src/main/java/ru/serp/corpwish/controller/AuthController.java
+++ backend/src/main/java/ru/serp/corpwish/controller/AuthController.java
@@ -1,16 +1,15 @@
 package ru.serp.corpwish.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.serp.corpwish.DTO.TelegramAuthRequest;
-import ru.serp.corpwish.service.AuthService;
-
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.serp.corpwish.DTO.TelegramAuthRequest;
+import ru.serp.corpwish.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,10 +18,22 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping
-    public ResponseEntity<?> authenticate(@RequestBody TelegramAuthRequest request)
-            throws NoSuchAlgorithmException, InvalidKeyException {
+    // NOTE: Использовать только для тестов, на проде убрать
+    @GetMapping
+    public ResponseEntity<Resource> getAuthScript() {
+        Resource resource = new ClassPathResource("auth.html");
+        var headers = new HttpHeaders();
+        headers.add(
+            HttpHeaders.CONTENT_DISPOSITION,
+            "inline; filename=auth.html"
+        );
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
 
+    @PostMapping
+    public ResponseEntity<?> authenticate(
+        @RequestBody TelegramAuthRequest request
+    ) throws NoSuchAlgorithmException, InvalidKeyException {
         String token = authService.authenticate(request);
 
         return ResponseEntity.ok(token);
