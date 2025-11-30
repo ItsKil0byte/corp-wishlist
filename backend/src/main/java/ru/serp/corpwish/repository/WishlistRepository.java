c17ea7f44aac7d48597935bc3629bc3bc599c1f2
Alexander Lebedev <itskilobyte@gmail.com>
2025-11-30 17:50:15 +0500

    Обновил репозиторий для загрузки списка вишлистов

--- backend/src/main/java/ru/serp/corpwish/repository/WishlistRepository.java
+++ backend/src/main/java/ru/serp/corpwish/repository/WishlistRepository.java
@@ -1,14 +1,27 @@
 package ru.serp.corpwish.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.serp.corpwish.entity.Wishlist;
+
+import java.util.List;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
-    // Как я понял из документации, Page нам может пригодиться, чтобы не грузить
-    // на фронт все данные, а лишь кусочек. Если пойму что фигня - переделаю
+    // Курсор - последний индекс элемента на экране. То есть как только
+    // кончилось место -> грузим ещё pageable штук, начиная с cursor.
 
-    Page<Wishlist> findByOwnerTelegramId(Long ownerId, Pageable pageable);
+    // Всё ещё не уверен, что оно нужно, но пусть будет на всякий.
+
+    @Query("""
+            SELECT wishlist FROM Wishlist wishlist
+            WHERE wishlist.owner.telegramId = :orderId
+                AND (:cursor IS NULL OR wishlist.id < :cursor)
+            ORDER BY wishlist.id DESC
+            """)
+    List<Wishlist> findNextWishlists(
+            @Param("ownerId") Long ownerId, @Param("cursor") Long cursor, Pageable pageable
+    );
 }
