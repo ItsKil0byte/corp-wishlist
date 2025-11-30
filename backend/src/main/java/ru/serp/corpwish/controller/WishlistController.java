11179992be60a591051a5ed0a0dbf699fe037bfd
Alexander Lebedev <itskilobyte@gmail.com>
2025-11-30 18:18:43 +0500

    Добавил новый эндпоинт в контроллер вишлистов

--- backend/src/main/java/ru/serp/corpwish/controller/WishlistController.java
+++ backend/src/main/java/ru/serp/corpwish/controller/WishlistController.java
@@ -1,6 +1,7 @@
 package ru.serp.corpwish.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,12 +11,30 @@ import ru.serp.corpwish.DTO.WishlistDto;
 import ru.serp.corpwish.entity.User;
 import ru.serp.corpwish.service.WishlistService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/wishlists")
 @RequiredArgsConstructor
 public class WishlistController {
 
     private final WishlistService wishlistService;
+
+    @GetMapping
+    public ResponseEntity<List<WishlistDto>> getWishlists(
+            @AuthenticationPrincipal User owner,
+            @RequestParam(required = false) Long cursor,
+            @RequestParam(defaultValue = "25") int limit
+    ) {
+        List<WishlistDto> wishlistsDto = wishlistService
+                .getWishlists(
+                        owner.getTelegramId(),
+                        cursor,
+                        PageRequest.of(0, limit)
+                );
+
+        return ResponseEntity.ok(wishlistsDto);
+    }
 
     @GetMapping("/{wishlistId}")
     public ResponseEntity<WishlistDto> getWishlist(
--- backend/src/main/java/ru/serp/corpwish/repository/WishlistRepository.java
+++ backend/src/main/java/ru/serp/corpwish/repository/WishlistRepository.java
@@ -17,7 +17,7 @@ public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
     @Query("""
             SELECT wishlist FROM Wishlist wishlist
-            WHERE wishlist.owner.telegramId = :orderId
+            WHERE wishlist.owner.telegramId = :ownerId
                 AND (:cursor IS NULL OR wishlist.id < :cursor)
             ORDER BY wishlist.id DESC
             """)
