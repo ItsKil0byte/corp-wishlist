afe4e9a578633af10c72b55b92a573c8ac668491
Alexander Lebedev <itskilobyte@gmail.com>
2025-12-08 17:26:55 +0500

    Создал контроллер для создания ссылок

---
+++ backend/src/main/java/ru/serp/corpwish/DTO/CreateLinkRequest.java
@@ -0,0 +1,13 @@
+package ru.serp.corpwish.DTO;
+
+import lombok.Data;
+import ru.serp.corpwish.entity.LinkType;
+
+import java.time.Instant;
+
+@Data
+public class CreateLinkRequest {
+    private LinkType type;
+    private Long entityId;
+    private Instant expiresAt;
+}
---
+++ backend/src/main/java/ru/serp/corpwish/controller/LinkController.java
@@ -0,0 +1,37 @@
+package ru.serp.corpwish.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import ru.serp.corpwish.DTO.CreateLinkRequest;
+import ru.serp.corpwish.DTO.InviteLinkDto;
+import ru.serp.corpwish.entity.User;
+import ru.serp.corpwish.service.LinkService;
+
+@RestController
+@RequestMapping("/api/links")
+@RequiredArgsConstructor
+public class LinkController {
+
+    private final LinkService linkService;
+
+    @PostMapping
+    public ResponseEntity<InviteLinkDto> createLink(
+            @AuthenticationPrincipal User user,
+            @RequestBody CreateLinkRequest request,
+            @RequestHeader(value = "X-Base-Url", required = false) String baseUrl
+    ) {
+        String url = (baseUrl != null) ? baseUrl : "todo";
+
+        InviteLinkDto dto = linkService.createLink(
+                user,
+                request.getType(),
+                request.getEntityId(),
+                request.getExpiresAt(),
+                url
+        );
+
+        return ResponseEntity.ok(dto);
+    }
+}
