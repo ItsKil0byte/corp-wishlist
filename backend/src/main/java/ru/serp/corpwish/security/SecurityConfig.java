898545c1a3686c5dbbad33f92a91d56e4e499f42
Alexander Lebedev <lebalexevg@gmail.com>
2026-03-30 15:56:32 +0500

    Прописал выдачу метрик по адресу

--- backend/src/main/java/ru/serp/corpwish/security/SecurityConfig.java
+++ backend/src/main/java/ru/serp/corpwish/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(authorize ->
                 authorize
-                    .requestMatchers("/api/auth/**", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs*/**", "/public/link/**")
+                    .requestMatchers("/api/auth/**", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs*/**", "/public/link/**", "/actuator/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
