0097f8616bf3d59a8a96561fe9ce4e7d931fa724
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-03 02:24:11 +0500

    (feat): Перенёс секцию с цифрами в компонент

---
+++ website/src/components/landing/Stats.jsx
@@ -0,0 +1,25 @@
+const stats = [
+  { val: "0", title: "Ненужных подарков" },
+  { val: "100%", title: "Попадание в цель" },
+  { val: "2 мин", title: "Вашего времени" },
+];
+
+export default function Stats() {
+  return (
+    <section className="bg-main-theme-lite rounded-lg p-12 text-center hover:-translate-y-1 transition-all shadow-sm">
+      <h2 className="text-3xl text-gray-900 font-bold mb-8">
+        Сделаем праздники в вашем отделе веселее!
+      </h2>
+      <div className="flex flex-wrap justify-center gap-12">
+        {stats.map((stat, index) => (
+          <div key={index} className="flex flex-col items-center">
+            <span className="text-4xl font-black text-main-theme">
+              {stat.val}
+            </span>
+            <span className="text-gray-600 mt-1">{stat.title}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
--- website/src/layouts/Root.jsx
+++ website/src/layouts/Root.jsx
@@ -1,6 +1,7 @@
 import Features from "@/components/landing/Features";
 import Header from "@/components/landing/Header";
 import Hero from "@/components/landing/Hero";
+import Stats from "@/components/landing/Stats";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
@@ -11,6 +12,7 @@ export default function Root() {
       <main className="flex-1 max-w-6xl w-full mx-auto px-4 py-8">
         <Hero />
         <Features />
+        <Stats />
       </main>
 
       <footer className="border-t-2 border-gray-100 py-6 text-center text-sm text-gray-500">
