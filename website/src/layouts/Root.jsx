ffc58cdbe06e01a955be70ad08827b8416a32594
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-03 02:41:00 +0500

    (feat): Сборка лендинга

--- website/src/layouts/Root.jsx
+++ website/src/layouts/Root.jsx
@@ -1,22 +1,11 @@
-import Features from "@/components/landing/Features";
-import Footer from "@/components/landing/Footer";
-import Header from "@/components/landing/Header";
-import Hero from "@/components/landing/Hero";
-import Stats from "@/components/landing/Stats";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <Header />
-
       <main className="flex-1 max-w-6xl w-full mx-auto px-4 py-8">
-        <Hero />
-        <Features />
-        <Stats />
+        <Outlet />
       </main>
-
-      <Footer />
     </div>
   );
 }
--- website/src/pages/Landing.jsx
+++ website/src/pages/Landing.jsx
@@ -1,7 +1,19 @@
+import Features from "@/components/landing/Features";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import Stats from "@/components/landing/Stats";
+
 export default function Landing() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="font-bold text-2xl">Страница лендинга (не готова)</h1>
+      <Header />
+      <main className="w-full max-w-6xl flex flex-col gap-24 px-6">
+        <Hero />
+        <Features />
+        <Stats />
+      </main>
+      <Footer />
     </div>
   );
 }
