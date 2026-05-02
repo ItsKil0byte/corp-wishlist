baf701a118c40237ef20c7083b931d212bd2856b
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-03 01:18:48 +0500

    (feat): Добавлен хук для статей из блога

---
+++ website/src/hooks/useBlog.js
@@ -0,0 +1,72 @@
+import { BlogService } from "@/services/BlogService";
+import { useEffect, useState } from "react";
+
+export function useList(page = 0, limit = 10) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    setLoading(true);
+
+    BlogService.getBlogList(page, limit)
+      .then((response) => {
+        if (isMounted) {
+          setData(response);
+        }
+      })
+      .catch((error) => {
+        if (isMounted) {
+          setError(
+            error.response?.data?.message ||
+              error.message ||
+              "Ошибка загрузки!",
+          );
+        }
+      })
+      .finally(() => {
+        isMounted = false;
+      });
+  }, [page, limit]);
+
+  return { data, loading, error };
+}
+
+export default function usePost(slug) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!slug) {
+      return;
+    }
+
+    let isMounted = true;
+
+    setLoading(true);
+
+    BlogService.getBlogPost(slug)
+      .then((response) => {
+        if (isMounted) {
+          setData(response);
+        }
+      })
+      .catch((error) => {
+        if (isMounted) {
+          setError(
+            error.response?.data?.status === 404
+              ? "Статья не найдена!"
+              : error.message,
+          );
+        }
+      })
+      .finally(() => {
+        isMounted = false;
+      });
+  }, [slug]);
+
+  return { data, loading, error };
+}
--- website/src/pages/BlogList.jsx
+++ website/src/pages/BlogList.jsx
@@ -1,4 +1,9 @@
+import { useList } from "@/hooks/useBlog";
+
 export default function BlogList() {
+  // const response = useList();
+  // console.log(response);
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-bold text-2xl">Список статей (не готов)</h1>
